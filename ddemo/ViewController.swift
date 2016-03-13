cf35cb89a61fd8ea9e5fb2ebca0c04bc602b2eb7
wantli <kevin@Mingliang-no-MacBook-Pro.local>
2016-03-13 11:04:15 +0900

    first
    first commit

--- ddemo/ViewController.swift
+++ ddemo/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+         btn.setTitle("aaga", forState: UIControlState.Normal)
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
